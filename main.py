dbb11b9438bb4fdbfdf78df610b8a4221a3af0ca
Vezy Sec <vezy.sec@gmail.com>
2026-01-24 14:59:29 +0100

    implmented api calling and metadata examination

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
 def main():
     print("Hello from ai-agent!")
+    client = genai.Client(api_key=api_key)
+    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=prompt)#
+
+    if response.usage_metadata is None:
+        raise RuntimeError("Something went wrong. LLM API response does not contain expected data.")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response:\n {response.text}")
 
 
 if __name__ == "__main__":
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key is None:
+        raise RuntimeError("No API Key registered!")
     main()
