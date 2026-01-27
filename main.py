1328b383d23708c28e63a5aecbac40dd030e30b3
Vezy Sec <vezy.sec@gmail.com>
2026-01-27 20:14:14 +0100

    added calcuator part from boots

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def main():
 
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
@@ -20,8 +21,10 @@ def main():
 
     if response.usage_metadata is None:
         raise RuntimeError("Something went wrong. LLM API response does not contain expected data.")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response:\n {response.text}")
 
 
